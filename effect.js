86c48f8c2dfdbdff7e38de9c34bc106a17112f92
Ryan Efendy <ryanefendy95@gmail.com>
2020-03-19 22:10:37 -0700

    commit2

--- effect.js
+++ effect.js
@@ -25,11 +25,13 @@ $('document').ready(function(){
 		$('#bulb_orange').addClass('bulb-glow-orange');
 		$('body').addClass('peach');
 		$(this).fadeOut('slow').delay(1000).promise().done(function(){
-			$('#play').fadeIn('slow');
+			$('#play1').fadeIn('slow');
+			$('#play2').fadeIn('slow');
+			$('#play3').fadeIn('slow');
 		});
 	});
-	$('#play').click(function(){
-		var audio = $('.song')[0];
+	$('#play1').click(function(){
+		var audio = $('.song1')[0];
         audio.play();
         $('#bulb_yellow').addClass('bulb-glow-yellow-after');
 		$('#bulb_red').addClass('bulb-glow-red-after');
@@ -39,6 +41,42 @@ $('document').ready(function(){
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
 		$('body').css('backgroud-color','#FFF');
 		$('body').addClass('peach-after');
+		$('#play2').fadeOut('slow');
+		$('#play3').fadeOut('slow');
+		$(this).fadeOut('slow').delay(1000).promise().done(function(){
+			$('#bannar_coming').fadeIn('slow');
+		});
+	});
+	$('#play2').click(function(){
+		var audio = $('.song2')[0];
+        audio.play();
+        $('#bulb_yellow').addClass('bulb-glow-yellow-after');
+		$('#bulb_red').addClass('bulb-glow-red-after');
+		$('#bulb_blue').addClass('bulb-glow-blue-after');
+		$('#bulb_green').addClass('bulb-glow-green-after');
+		$('#bulb_pink').addClass('bulb-glow-pink-after');
+		$('#bulb_orange').addClass('bulb-glow-orange-after');
+		$('body').css('backgroud-color','#FFF');
+		$('body').addClass('peach-after');
+		$('#play1').fadeOut('slow');
+		$('#play3').fadeOut('slow');
+		$(this).fadeOut('slow').delay(1000).promise().done(function(){
+			$('#bannar_coming').fadeIn('slow');
+		});
+	});
+	$('#play3').click(function(){
+		var audio = $('.song3')[0];
+        audio.play();
+        $('#bulb_yellow').addClass('bulb-glow-yellow-after');
+		$('#bulb_red').addClass('bulb-glow-red-after');
+		$('#bulb_blue').addClass('bulb-glow-blue-after');
+		$('#bulb_green').addClass('bulb-glow-green-after');
+		$('#bulb_pink').addClass('bulb-glow-pink-after');
+		$('#bulb_orange').addClass('bulb-glow-orange-after');
+		$('body').css('backgroud-color','#FFF');
+		$('body').addClass('peach-after');
+		$('#play1').fadeOut('slow');
+		$('#play2').fadeOut('slow');
 		$(this).fadeOut('slow').delay(1000).promise().done(function(){
 			$('#bannar_coming').fadeIn('slow');
 		});
